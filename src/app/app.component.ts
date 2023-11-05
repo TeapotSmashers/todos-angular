4b7e2f18b847b4ec0a0cf150d11ee9d37d5d6222
Sankalp Mukim <sankalpmukim@gmail.com>
2023-11-05 19:32:10 +0530

    state working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Session } from '@supabase/supabase-js';
+import { SupabaseService } from './supabase.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todos-angular';
+  loading = true;
+  session: Session | null = null;
+
+  constructor(supabaseService: SupabaseService) {
+    supabaseService.getSession().then((v) => {
+      this.session = v;
+      this.loading = false;
+    });
+  }
 }
